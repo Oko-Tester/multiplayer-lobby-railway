7a27f83347838538e8eb3fb76a8ac645ba501ede
Oko-Tester <remeralexander@gmail.com>
2025-08-09 16:15:10 +0200

    entrypoint: updated

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -3,11 +3,6 @@
 cat > /usr/share/nginx/html/env.js << EOF
 window._env_ = {
   SERVER_URL: "${SERVER_URL:+https://$SERVER_URL}",
-  SERVER_PRIVATE_DOMAIN: "${RAILWAY_PRIVATE_DOMAIN:-}",
-  RAILWAY_ENVIRONMENT: "${RAILWAY_ENVIRONMENT:-production}",
-  RAILWAY_PROJECT_NAME: "${RAILWAY_PROJECT_NAME:-}",
-  RAILWAY_SERVICE_NAME: "${RAILWAY_SERVICE_NAME:-}",
-  NODE_ENV: "${NODE_ENV:-production}"
 };
 EOF
 
